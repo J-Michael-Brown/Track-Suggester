23afd7221f2b5782062427399b445ed33ebc254f
John Michael Brown <jmichaelbrown132737@gmail.com>
2018-10-19 12:15:53 -0700

    assign elements to expected positions of function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 var match  = function(ruby, java, cSharp, php) {
-  // var sub5 = parseInt($("subject5").val()); No info for CSS/Design yet
+  // No info for CSS/Design yet
 
   $(".result").hide();
-
 
   if (ruby > php && ruby === java && ruby === cSharp || ruby > java && ruby === php && ruby === cSharp || ruby > cSharp && ruby === java && ruby === php || php > ruby && php === java && php === cSharp || php > java && php === ruby && php === cSharp || php > cSharp && php === java && php === ruby || ruby === java && ruby === cSharp && ruby === php) { // accounts for three way ties that include ruby or php
     $(".result#unsure").show();
@@ -86,68 +85,73 @@ $(document).ready(function() {
     var companyInput = $("input:radio[name=company-type]:checked").val();
     var endInput = $("input:radio[name=end-type]:checked").val();
     var appInput = $("input:radio[name=app-type]:checked").val();
-    var fourInput = $("input:radio[name=four-type]:checked").val();
-    var fifthInput = $("input:radio[name=four-type]:checked").val();
+    var fourthInput = $("input:radio[name=four-type]:checked").val();
+    var fifthInput = $("input:radio[name=five-type]:checked").val();
 
     var rubyRails = 0;
     var phpDrupal = 0;
     var javaAndroid = 0;
     // var cssDesign = 0;
-    var CsharpDotNet = 0;
+    var cSharpDotNet = 0;
+
 
     if (companyInput === "big") {
-      CsharpDotNet += 1;
-    } else if (companyInput === "upstart") {
+      cSharpDotNet += 1;
+    }
+    if (companyInput === "upstart") {
       rubyRails += 1;
-    } else if (companyInput === "flex") {
+    }
+    if (companyInput === "flex") {
       phpDrupal += 1;
     }
+
     if (endInput === "front") {
       rubyRails += 1;
-    } else if (endInput === "back") {
-      CsharpDotNet += 1;
-      // phpDrupal += 1;
-    } else if (endInput === "unsure") {
+    }
+    if (endInput === "back") {
+      cSharpDotNet += 1;
+      phpDrupal += 1;
+    }
+    if (endInput === "unsure") {
       javaAndroid += 1;
     }
+
     if (appInput === "android") {
       javaAndroid += 1;
-    } else if (appInput === "solve") {
-      CsharpDotNet += 1;
+    }
+    if (appInput === "solve") {
+      cSharpDotNet += 1;
       phpDrupal += 1;
-    } else if (appInput === "interactive") {
+    }
+    if (appInput === "interactive") {
       rubyRails += 1;
     }
-    if (fourInput === "one") {
 
-    } else if (appInput === "two") {
-
-    } else if (appInput === "three") {
-
+    if (fourthInput === "CnN") {
+      cSharpDotNet += 2;
     }
-    if (fifthInput === "one") {
-
-    } else if (appInput === "two") {
-
-    } else if (appInput === "three") {
-
+    if (fourthInput === "JnA") {
+      javaAndroid += 2;
+    }
+    if (fourthInput === "PnD") {
+      phpDrupal += 2;
+    }
+    if (fourthInput === "RnR") {
+      rubyRails += 2;
     }
 
-    match(rubyRails, CsharpDotNet, phpDrupal, javaAndroid);
-    // $(".result-area").show();
-    // if (shaggy > monster+snow) {
-    //   $(".result").hide();
-    //   $("#shaggy").show();
-    // } else if (monster > shaggy+snow) {
-    //   $(".result").hide();
-    //   $("#monster").show();
-    // } else if (snow > shaggy+monster) {
-    //   $(".result").hide();
-    //   $("#snow").show();
-    // } else {
-    //   $(".result").hide();
-    //   $("#no-one").show();
-    // }
+    if (fifthInput === "what") {
+      javaAndroid += 1;
+    }
+    if (fifthInput === "who") {
+      rubyRails += 1;
+    }
+    if (fifthInput === "how") {
+      phpDrupal += 1;
+      cSharpDotNet += 1;
+    }
+
+    match(rubyRails, javaAndroid, cSharpDotNet, phpDrupal);
 
   });
 
